98b6aa0e322adf8a536181c87cbf654c6d369cf6
신형재 <newnewbrother@gmail.com>
2018-02-12 18:37:56 +0900

    Initial Commit

---
+++ cau_studyTests/cau_studyTests.swift
@@ -0,0 +1,36 @@
+//
+//  cau_studyTests.swift
+//  cau_studyTests
+//
+//  Created by 신형재 on 2018. 2. 12..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import XCTest
+@testable import cau_study
+
+class cau_studyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
