1e70647d086380bcda1ca2fbf483ed8305aa43c4
신형재 <newnewbrother@gmail.com>
2018-02-14 01:23:00 +0900

    first

---
+++ cau_study/viewController/studyViewController.swift
@@ -0,0 +1,41 @@
+//
+//  studyViewController.swift
+//  cau_study
+//
+//  Created by 신형재 on 2018. 2. 12..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import UIKit
+
+class studyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = true
+        self.navigationItem.title = "스터디그룹"
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
