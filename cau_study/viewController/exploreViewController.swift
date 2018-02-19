290d83b5660a4cbb94c0725252d9c32ee7955a78
Newbrother <newnewbrother@gmail.com>
2018-02-19 19:52:06 +0900

    debugged

---
+++ cau_study/viewController/exploreViewController.swift
@@ -0,0 +1,40 @@
+//
+//  exploreViewController.swift
+//  cau_study
+//
+//  Created by 신형재 on 19/02/2018.
+//  Copyright © 2018 신형재. All rights reserved.
+//
+
+import UIKit
+
+class exploreViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
