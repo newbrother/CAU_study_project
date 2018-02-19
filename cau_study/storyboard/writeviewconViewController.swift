3c25fabf1487255b536d64d519c297f5918620f8
정다예 <jeongdaye@jeongdayeui-macbook-pro.local>
2018-02-19 22:15:25 +0900

    make write and search viewcontroller

---
+++ cau_study/storyboard/writeviewconViewController.swift
@@ -0,0 +1,38 @@
+//
+//  writeviewconViewController.swift
+//  cau_study
+//
+//  Created by 정다예 on 2018. 2. 19..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import UIKit
+
+class writeviewconViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
