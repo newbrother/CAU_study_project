08a4b2996112f3c5aabcf1aa2b74dea5f86b7cdf
Dahye <dahyedarling@gmail.com>
2018-02-26 19:46:59 +1100

    add cocoaTouchClass for feedWritingView

---
+++ cau_study/viewController/feedWritingViewController.swift
@@ -0,0 +1,19 @@
+//
+//  feedWritingViewController.swift
+//  cau_study
+//
+//  Created by Davee on 2018. 2. 26..
+//  Copyright © 2018년 신형재. All rights reserved.
+//
+
+import UIKit
+
+class feedWritingViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
