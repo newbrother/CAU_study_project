a7ea23cf972a3a0bf65302a7de147eac847786d3
wr <msfamzzang@naver.com>
2018-03-06 16:47:00 +0900

    photo
    photo

--- cau_study/viewController/PostViewController.swift
+++ cau_study/viewController/PostViewController.swift
@@ -10,30 +10,37 @@ import UIKit
 
 class PostViewController: UIViewController {
 
+    @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var post: UITextView!
     @IBOutlet weak var shareButton: UIButton!
+    var selectedImage: UIImage?
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(self.handleSelectPhoto))
+        photo.addGestureRecognizer(tapGesture)
+        photo.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
+    }
+    
+    @objc func handleSelectPhoto() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        present(pickerController, animated: true, completion: nil)
     }
 
     @IBAction func shareButton_TouchUpInside(_ sender: Any) {
     }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+}
+
+extension PostViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("ddd")
+        if let image = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            selectedImage = image
+            photo.image = image
+        }
+        dismiss(animated: true, completion: nil)
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
